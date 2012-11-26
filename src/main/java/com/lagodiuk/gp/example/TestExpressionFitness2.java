c7d36006ad6cd456840d3a5d182608855c527243
Лагодюк Юрий <cafebabe@yandex-team.com.ua>
2012-11-26 20:09:55 +0200

    New experiments

--- src/main/java/com/lagodiuk/gp/example/TestExpressionFitness2.java
+++ src/main/java/com/lagodiuk/gp/example/TestExpressionFitness2.java
@@ -14,11 +14,11 @@ public class TestExpressionFitness2 implements ExpressionFitness {
 			for (int y = -5; y < 5; y++) {
 				context.setVariable("y", y);
 
-				double target = (x * 5) + (y * (y - 4));
+				// double target = (x * 5) + (y * (y - 4));
 				// double target = x + y;
 				// double target = x * 5 + y * ( y - 4 ) + x * y;
 				// double target = x * x;
-				// double target = (x * x) + (y * y);
+				double target = (x * x) + (y * y);
 
 				double val = target - expression.eval(context);
 
