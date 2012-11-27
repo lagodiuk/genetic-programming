f40f3eeb77049a7872d0a58f94e9b6c3b1122fa5
Лагодюк Юрий <cafebabe@yandex-team.com.ua>
2012-11-27 18:49:28 +0200

    Principal component analysis experiment + 2 new f(x,y) experiments

--- src/main/java/com/lagodiuk/gp/example/TestExpressionFitness2.java
+++ src/main/java/com/lagodiuk/gp/example/TestExpressionFitness2.java
@@ -9,16 +9,16 @@ public class TestExpressionFitness2 implements ExpressionFitness {
 	@Override
 	public double fitness(Expression expression, Context context) {
 		double delt = 0;
-		for (int x = -5; x < 5; x++) {
+		for (int x = -10; x < 10; x += 2) {
 			context.setVariable("x", x);
-			for (int y = -5; y < 5; y++) {
+			for (int y = -10; y < 10; y += 2) {
 				context.setVariable("y", y);
 
 				// double target = (x * 5) + (y * (y - 4));
 				// double target = x + y;
-				// double target = x * 5 + y * ( y - 4 ) + x * y;
+				double target = (x * 5) + (y * (y - 4)) + (x * y);
 				// double target = x * x;
-				double target = (x * x) + (y * y);
+				// double target = (x * x) + (y * y);
 
 				double val = target - expression.eval(context);
 
